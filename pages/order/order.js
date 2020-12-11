adff6f79d5e52a323ae53747f8aa6a5bd8b8692b
Hirate99 <kevinsu99@pku.edu.cn>
2020-12-11 18:19:44 +0800

    实现维修单页面的示例
    包括了wxml，wxss和简单的仅限于前端部分的js

--- pages/order/order.js
+++ pages/order/order.js
@@ -1,3 +1,5 @@
+import Dialog from '@vant/weapp/dialog/dialog';
+
 Page({
 
   /**
@@ -11,33 +13,152 @@ Page({
     ],
     value1: 0,
     activeNames: ['1'],
+    restoredReq: '',
+    restoredAct: -1,
+    List: [
+      {
+        iconPath: '/pages/order/rejectedOrder.png',
+        num: 1,
+        date: '1970-01-01',
+        pcmodule: 'None',
+        requests: 'None',
+        status: 'REJECTED',
+        orderDetails: 'None',
+        reply: '已拒绝'
+      },
+      {
+        iconPath: '/pages/order/comfirmedOrder.png',
+        num: 2,
+        date: '2020-12-25',
+        pcmodule: 'xps-13',
+        requests: '更换电池',
+        status: 'ACCEPTED',
+        orderDetails: '更换电池',
+        reply: '已拒绝'
+      },
+      {
+        iconPath: '/pages/order/comfirmedOrder.png',
+        num: 3,
+        date: '2020-12-25',
+        pcmodule: 'xps-13',
+        requests: '更换电池',
+        status: 'ACCEPTED',
+        orderDetails: '更换电池',
+        reply: '已拒绝'
+      },
+      {
+        iconPath: '/pages/order/comfirmedOrder.png',
+        num: 4,
+        date: '2020-12-25',
+        pcmodule: 'xps-13',
+        requests: '更换电池',
+        status: 'ACCEPTED',
+        orderDetails: '更换电池',
+        reply: '已拒绝'
+      },
+      {
+        iconPath: '/pages/order/comfirmedOrder.png',
+        num: 5,
+        date: '2020-12-25',
+        pcmodule: 'xps-13',
+        requests: '更换电池',
+        status: 'ACCEPTED',
+        orderDetails: '更换电池',
+        reply: '已拒绝'
+      },
+      {
+        iconPath: '/pages/order/comfirmedOrder.png',
+        num: 6,
+        date: '2020-12-25',
+        pcmodule: 'xps-13',
+        requests: '更换电池',
+        status: 'ACCEPTED',
+        orderDetails: '更换电池',
+        reply: '已拒绝'
+      },
+      {
+        iconPath: '/pages/order/comfirmedOrder.png',
+        num: 7,
+        date: '2020-12-25',
+        pcmodule: 'xps-13',
+        requests: '更换电池',
+        status: 'ACCEPTED',
+        orderDetails: '更换电池',
+        reply: '已拒绝'
+      },
+      {
+        iconPath: '/pages/order/comfirmedOrder.png',
+        num: 8,
+        date: '2020-12-25',
+        pcmodule: 'xps-13',
+        requests: '更换电池',
+        status: 'ACCEPTED',
+        orderDetails: '更换电池',
+        reply: '已拒绝'
+      },
+    ],
   },
 
   onChange(event) {
-    this.setData({
-      activeNames: event.detail,
-    });
+    console.log(event)
+    if (this.data.restoredAct === -1) {
+      this.setData({
+        activeNames: event.detail,
+        restoredAct: event.detail,
+        restoredReq: this.data.List[event.detail].requests,
+        ['List['+event.detail+'].requests']: ''
+      });
+    } else {
+      this.setData({
+        activeNames: event.detail,
+        ['List['+this.data.restoredAct+'].requests']: this.data.restoredReq,
+        restoredAct: event.detail,
+        restoredReq: this.data.List[event.detail].requests,
+        ['List['+event.detail+'].requests']: ''
+      });
+    }
   },
 
+/**
+ * 需要更改！
+ */
+  onClose(event) {
+    const { position, instance } = event.detail;
+    console.log(event.detail)
+    switch (position) {
+      case 'left':
+      case 'cell':
+        instance.close();
+        break;
+      case 'right':
+        Dialog.confirm({
+          message: '确定删除吗？',
+        }).then(() => {
+          instance.close()
+        });
+        break;
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
+   * 需要更改！
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
