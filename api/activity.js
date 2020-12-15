bab5d85de72a24f1e1ce0825f62bbcbd1b154c19
beerDuc <pjy@pku.edu.cn>
2020-12-15 13:45:24 +0800

    适应requestWithToken的修改，更改了activity API

--- api/activity.js
+++ api/activity.js
@@ -2,8 +2,6 @@ import {requestWithToken} from "./user";
 import {wxp} from '../utils/wxp';
 //获取未开始活动列表
 export const getCurrentActivities = async function(currentTime,filter){
-  let app = getApp()
-  let baseURL = app.globalData.baseURL
   var data={
     endLower: currentTime,
     startUpper:currentTime
@@ -11,22 +9,20 @@ export const getCurrentActivities = async function(currentTime,filter){
   var requestRes
   if(filter){
     console.log("filter")
-    requestRes = await requestWithToken(baseURL + '/api/activity','GET',data)
+    requestRes = await requestWithToken('/api/activity','GET',data)
   }
   else{
     console.log("no filter")
-    requestRes = await requestWithToken(baseURL + '/api/activity','GET')
+    requestRes = await requestWithToken('/api/activity','GET')
   }
   return requestRes
 }
 
 export const getActivitySlot = async function(actId){
-  let app = getApp()
-  let baseURL = app.globalData.baseURL
   let data={
     "activity":actId
   }
-  let requestRes = await requestWithToken(baseURL + '/api/activity/slot','GET',data)
+  let requestRes = await requestWithToken('/api/activity/slot','GET',data)
   return requestRes
 }
 
